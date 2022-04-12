ba4525a84e46bd99bfa1e47ac8685490e2472524
networkninjadh <networkninjadh@gmail.com>
2022-04-12 12:15:03 -0500

    initial commit

---
+++ src/test/java/com/howtech/posauthapi/PosAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howtech.posauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
